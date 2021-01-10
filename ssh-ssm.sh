70bfe4ec6fafbda4108e19441cf3d79978c9a620
Sherif Abdel-Naby <sherifabdlnaby@gmail.com>
2021-01-10 13:46:51 +0200

    Update Script.
    1. Don't Delete Key from local machine (avoid some concurrency corner cases)
    2. Use SED to delete only one occurrence of the public key on the host machine (again to avoid problems when using multiple sessions concurrently.)

--- ssh-ssm.sh
+++ ssh-ssm.sh
@@ -1,49 +1,55 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
-function main {
-  local ssh_pubkey ssm_cmd
-  local ssh_authkeys='.ssh/authorized_keys'
-  ssh_dir=~/.ssh
+# This is a modifed script of a open-source helper script: https://github.com/elpy1/ssh-over-ssm
 
-  checks "$@" && ssh_pubkey=$(ssh-add -L 2>/dev/null| head -1) || mktmpkey
-  ssm_cmd=$(cat <<EOF
-    "u=\$(getent passwd ${2}) && x=\$(echo \$u |cut -d: -f6) || exit 1
-    install -d -m700 -o${2} \${x}/.ssh; grep '${ssh_pubkey}' \${x}/${ssh_authkeys} && exit 1
-    printf '${ssh_pubkey}'|tee -a \${x}/${ssh_authkeys} && sleep 15
-    sed -i s,'${ssh_pubkey}',, \${x}/${ssh_authkeys}"
+# ----------------------------------------  GET INSTANCE ID IF GIVEN INPUT IS NOT ID ----------------------------------------
+
+instance=${1}
+
+
+# ----------------------------------------  ADD PUBLIC KEY TO REMOTE INSTANCE ----------------------------------------
+
+# Check If SSM key is on client, create it if it doesn't exist.
+if [ ! -f ~/.ssh/ssm-ssh-key.pub ] || [ ! -f ~/.ssh/ssm-ssh-key ]
+then    
+  ssh-keygen -t ed25519 -N '' -f ~/.ssh/ssm-ssh-key -C ssm-ssh-session-$USER <<< yes
+fi
+
+# Public Key Value that we're going to set at the remote instance
+ssh_pubkey="$(< ~/.ssh/ssm-ssh-key.pub)"
+
+# The Command that is going to run at the remote instance to ad the key.
+# - Notice that the expression with escaped \${var} is going to be expaned AT the remote instance.
+ssm_cmd=$(cat <<EOF
+  "
+  # Get User Home Directory (cant use ~)
+  user=\$(getent passwd ${2}) && home=\$(echo \$user |cut -d: -f6)
+  # Create Folder for the user if it doesn't exist
+  install -d -m700 -o${2} \${home}/.ssh
+  # Add Public Key to Authorized Keys and Wait 15 Seconds
+  echo '${ssh_pubkey}' >> \${home}/.ssh/authorized_keys && sleep 15;
+  # Delete the Key
+  sed -i '0,\,${ssh_pubkey},{//d;}' \${home}/.ssh/authorized_keys
+  "
 EOF
-  )
+)
 
-  # put our public key on the remote server
-  aws ssm send-command \
-    --instance-ids "$1" \
-    --document-name "AWS-RunShellScript" \
-    --parameters commands="${ssm_cmd}" \
-    --comment "temporary ssm ssh access" #--debug
 
-  # start ssh session over ssm
-  aws ssm start-session --document-name AWS-StartSSHSession --target "$1" #--debug
-}
+# Send The Command to the Remote Instance to run instantly (and asyncrhonusly)
+# - The Command puts the key and deletes it after 15 seconds, we only need it to be present only when we run the below ssm command.
+aws ssm send-command \
+  --instance-ids "$instance" \
+  --document-name "AWS-RunShellScript" \
+  --parameters commands="${ssm_cmd}" \
+  --comment "Adding Temp Access Key for USER:$USER" || (echo "Invalid Instance ID: $instance. Make sure you have permission to instance." >&2 && exit 1);
 
-function checks {
-  [[ $# -ne 2 ]] && die "Usage: ${0##*/} <instance-id> <ssh user>"
-  [[ ! $1 =~ ^i-([0-9a-f]{8,})$ ]] && die "ERROR: invalid instance-id"
-  if [[ $(basename -- $(ps -o comm= -p $PPID)) != "ssh" ]]; then
-    ssh -o IdentityFile="~/.ssh/ssm-ssh-tmp" -o ProxyCommand="${0} ${1} ${2}" "${2}@${1}"
-    exit 0
-  fi
-  pr="$(grep -sl --exclude='*tool-env' "$1" "${ssh_dir}"/ssmtool-*)" &&
-  export AWS_PROFILE=${AWS_PROFILE:-${pr##*ssmtool-}}
-}
+# Sleep for some time to avoid if the above script didn't run instantly (Although the SSM Send-Command returns when the script HAS Started)
+sleep 1
 
-function mktmpkey {
-  trap cleanup EXIT
-  ssh-keygen -t ed25519 -N '' -f "${ssh_dir}"/ssm-ssh-tmp -C ssm-ssh-session
-  ssh_pubkey="$(< "${ssh_dir}"/ssm-ssh-tmp.pub)"
-}
-
-function cleanup { rm -f "${ssh_dir}"/ssm-ssh-tmp{,.pub}; }
-function die { echo "[${0##*/}] $*" >&2; exit 1; }
-
-main "$@"
+# Start SSH Session over SSM
+# - Session Manager is going to proxy our SSH Client traffic(that is running this script) to the SSH on the host.
+# - This Script does the inital setup and then hands sending ssh traffic to the ssh client that invoked it via `ssh` command.
+#   <Client SSH> ---ssh-traffic--> <This Script> ---ssh-traffic--> <SSM> ---ssh-traffic--> <Remote Host SSH>
+# - SSM let us reach the Remote Instances even if they're in a private subnet.
+aws ssm start-session --document-name AWS-StartSSHSession --target "$instance"
