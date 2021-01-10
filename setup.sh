9e30b916fe4808d0cb608f8c2a979edbdd18e6d3
Sherif Abdel-Naby <sherifabdlnaby@gmail.com>
2021-01-10 14:06:06 +0200

    Add Setup Script + Update README.md

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 set -o nounset -o pipefail -o errexit
 
 # Download Script
-curl https://raw.githubusercontent.com/sherifabdlnaby/aws-ssm-ssh-util/master/aws-ssm-ssh-util.sh >> ~/.ssh/aws-ssm-ssh-util
+curl -o- -s https://raw.githubusercontent.com/sherifabdlnaby/aws-ssm-ssh-util/master/aws-ssm-ssh-util.sh >> ~/.ssh/aws-ssm-ssh-util
 chmod +x ~/.ssh/aws-ssm-ssh-util
 echo "✅ Added aws-ssm-ssh-util script to SSH folder"
 
 # Add SSH Config
-curl https://raw.githubusercontent.com/sherifabdlnaby/aws-ssm-ssh-util/master/ssh-config >> ~/.ssh/config
+curl -o- -s https://raw.githubusercontent.com/sherifabdlnaby/aws-ssm-ssh-util/master/ssh-config >> ~/.ssh/config
 echo "✅ Added aws-ssm-ssh-util config to SSH Config"
