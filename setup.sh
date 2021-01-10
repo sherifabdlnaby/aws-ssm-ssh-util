043381d47e1d54f7e80f847e8966b0ebcd2422c2
Sherif Abdel-Naby <sherifabdlnaby@gmail.com>
2021-01-10 14:00:30 +0200

    Add Setup Script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+set -o nounset -o pipefail -o errexit
+
+# Download Script
+curl https://raw.githubusercontent.com/sherifabdlnaby/aws-ssm-ssh-util/master/aws-ssm-ssh-util.sh >> ~/.ssh/aws-ssm-ssh-util
+chmod +x ~/.ssh/aws-ssm-ssh-util
+echo "✅ Added aws-ssm-ssh-util script to SSH folder"
+
+# Add SSH Config
+curl https://raw.githubusercontent.com/sherifabdlnaby/aws-ssm-ssh-util/master/ssh-config >> ~/.ssh/config
+echo "✅ Added aws-ssm-ssh-util config to SSH Config"
